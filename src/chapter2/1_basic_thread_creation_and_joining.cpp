0a02514811ae22736b99131bc04254486db37a1d
Nikodem Czarlinski <nczarlinski@gmail.com>
2024-12-15 21:36:13 +0000

    Add chapter 2 first experiment

---
+++ src/chapter2/1_basic_thread_creation_and_joining.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <thread>
+
+void print_message(const std::string& message) {
+    for (int i = 0; i < 5; ++i) {
+        std::cout << message << " - iteration " << i + 1 << "\n";
+    }
+}
+
+int main() {
+    std::thread thread1(print_message, "Thread 1 running");
+    std::thread thread2(print_message, "Thread 2 running");
+
+    thread1.join();
+    thread2.join();
+
+    std::cout << "Main thread finished.\n";
+    return 0;
+}
